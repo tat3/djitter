bd431cf27f5258165cc9eab752b4cbe0398b879e
tat3 <n.tatsuki.214@gmail.com>
2018-06-26 23:21:31 +0900

    Fix a bug about dependency

--- djitter/urls.py
+++ djitter/urls.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import handler404
 
-from .views import page_not_found
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
