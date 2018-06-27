459655415555412695933c509980f90eb2f88be5
tat3 <n.tatsuki.214@gmail.com>
2018-06-27 22:39:33 +0900

    Invite more people

--- djitter/src/generate_fixture.py
+++ djitter/src/generate_fixture.py
@@ -1,8 +1,11 @@
 import json
+import random
 
 
 class FixtureGenerator:
 
+    skip = 3
+    
     def g_user_object(self, n):
         item = {
             "model": "auth.user",
@@ -13,13 +16,16 @@ class FixtureGenerator:
         }
         return item
 
-    def g_profile_object(self, n):
+    def g_profile_object(self, n, m):
+        skip = self.skip
+        l = list(filter(lambda x: not x == n, [i for i in range(skip, m + skip)]))
+        follows = random.sample(l, m // 2)
         item = {
             "model": "djeeterprofile.djeeterprofile",
             "pk": n,
             "fields": {
                 "user": n,
-                "follows": []
+                "follows": follows
             },
         }
         return item
@@ -36,10 +42,10 @@ class FixtureGenerator:
         }
         return item
 
-    def gen_fixture(self, m, d):
-        skip = 3
+    def g_fixture(self, m, d):
+        skip = self.skip
         items = [self.g_user_object(i) for i in range(skip, m + skip)]
-        items += [self.g_profile_object(i) for i in range(skip, m + skip)]
+        items += [self.g_profile_object(i, m) for i in range(skip, m + skip)]
         for i in range(m):
             items += [self.g_djeet_object(i * d + j + skip, i + skip, "hoge") for j in range(d)]
         return json.dumps(items, indent=4)
@@ -48,7 +54,7 @@ class FixtureGenerator:
 def generate_fixture_file(path):
     generator = FixtureGenerator()
     with open(path, "w") as f:
-        f.write(generator.gen_fixture(5, 10))
+        f.write(generator.g_fixture(100, 10))
 
 if __name__ == "__main__":
     
