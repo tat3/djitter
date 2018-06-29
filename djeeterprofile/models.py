d985759364f238dfb8fd5d1bcebc9389ea246641
tat3 <n.tatsuki.214@gmail.com>
2018-06-29 15:41:59 +0900

    Fix a word at the follow button

--- djeeterprofile/models.py
+++ djeeterprofile/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class DjeeterProfile(models.Model):
-  user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
-  follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False)
+    user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
+    follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False)
 
 User.djeeterprofile = property(lambda u: DjeeterProfile.objects.get_or_create(user=u)[0])
