fb4da85d55c926a28eea61b2af3feea6f6a1c51f
tat3 <n.tatsuki.214@gmail.com>
2018-06-30 20:36:47 +0900

    Fix for heroku

--- djitter/settings.py
+++ djitter/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('PRODUCTION') != 'True')
 
-ALLOWED_HOSTS = ["localhost", "djitter2.herokuapp.com"]
+ALLOWED_HOSTS = ["localhost", "app", "djitter2.herokuapp.com"]
 
 
 # Application definition
